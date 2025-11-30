39dd302d8e08c7e0606d28f224e05a3c2612bb67
Ghadeer Fallatah <ghadeer1781@gmail.com>
2025-11-30 09:30:21 +0300

    Initial Commit

---
+++ MaseerApp/MaseerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaseerAppApp.swift
+//  MaseerApp
+//
+//  Created by Ghadeer Fallatah on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MaseerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
